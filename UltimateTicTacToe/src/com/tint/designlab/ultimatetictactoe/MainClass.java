80fc846b574171fd4003580903fad8bf5c2d25db
ankita <ankitabasu.8420539497@gmail.com>
2021-06-27 15:49:55 +0530

    added

---
+++ UltimateTicTacToe/src/com/tint/designlab/ultimatetictactoe/MainClass.java
@@ -0,0 +1,18 @@
+package com.tint.designlab.ultimatetictactoe;
+
+/**
+ * 
+ * @author ANKITA BASU,
+ * 		   AAKANKSHA JENA,
+ * 		   SUPARNA BANERJEE,
+ *         SHENHA DEB,
+ *         AYANTIKA SARKAR
+ *
+ */
+public class MainClass {
+	public static void main(String[] args)
+	{
+		UltimateTicTacToe  pictureGui = new UltimateTicTacToe();
+		pictureGui.setVisible(true);
+	}
+}
